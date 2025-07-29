b39c603dd69ad7f451db9936c865c4fde18e019d
itsRKM2604 <rishavmishra2000@gmail.com>
2025-07-29 22:01:36 +0530

    Pushing the ToDo List to the git repository

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const body_parser = require("body-parser");
+port = 5454;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+var items = [];
+
+app.get("/", (req, res) => {
+  res.render("list", { taskejs: items });
+});
+
+app.post("/", (req, res) => {
+  let task = req.body.task;
+  let status = false;
+  items.forEach((item) => {
+    if (item.toLowerCase() == task.toLowerCase()) {
+      status = true;
+    }
+  });
+  if (!status) {
+    items.push(task);
+  }
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
