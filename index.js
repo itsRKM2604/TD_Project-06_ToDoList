d244b8e68287b814a9528558569c965423e546da
itsRKM2604 <rishavmishra2000@gmail.com>
2025-07-30 03:10:45 +0530

    Pushing the ToDo List to the git repository

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const body_parser = require("body-parser");
-port = 5454;
+// port = 5454;
+port = process.env.PORT || 8080;
+
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
